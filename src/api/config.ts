fa29dc33e5afbaa0a66942f3c823bca4e2ecdee7
vikasgupta21 <vvarshney777@gmail.com>
2025-11-23 02:24:29 +0530

    fixed geo code api

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
        BASE_URL : "https://api.openweathermap.org/data/2.5",
-       GEO : "http://api.openweathermap.org/geo/1.0",
+       GEO : "https://api.openweathermap.org/geo/1.0",
        API_KEY : import.meta.env.VITE_OPEN_WEATHER_API_KEY,
        DEAFULT_PARAMS :{
            units: "metric",
